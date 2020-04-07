a69ead17f795c5a16af90a135f7a349d913f4ed8
ederfmatos <ederfmatos@gmail.com>
2020-04-07 19:00:25 -0300

    Configurando estrutura

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+	],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier', 'react-hooks'],
+	rules: {
+		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': [
+			'warn',
+			{
+				extensions: ['.jsx', '.js'],
+			},
+		],
+		'import/prefer-default-export': 'off',
+		'no-param-reassign': 'off',
+		'no-console': ['error', { allow: 'tron' }],
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+	},
+};
